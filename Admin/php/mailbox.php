f100bb0b5506f4a0c32f701bc48f22f9afdd4e0f
Gilbert <mikamickswonder@gmail.com>
2021-04-25 21:48:02 +0300

    andrana contact
    niezaka nanao anle contact ashynchrone za fa mbola lasa tsy nety ndray ilay izy

--- Admin/php/mailbox.php
+++ Admin/php/mailbox.php
@@ -1,26 +1,20 @@
 <?php
+$success = 0;
+$msg = "une erreur est survenue(mailbox.php)!";
 include 'connexion.php';
- if (isset($_POST['envoyer'])) {
- 	if (!empty($_POST['name']) AND !empty($_POST['email']) AND !empty($_POST['objet']) AND !empty($_POST['msg']) ) {
- 		if (isset($_POST['name']) AND isset($_POST['email']) AND isset($_POST['objet']) AND isset($_POST['msg']) ) {
- 	$nom = $_POST['name'];
- 	$email = $_POST['email'];
- 	$objet = $_POST['objet'];
- 	$msg = $_POST['msg'];
+
+ 		if (isset($_POST['name']) AND isset($_POST['email']) AND isset($_POST['objet']) AND isset($_POST['msg'])) {
+	 	$nom = $_POST['name'];
+	 	$email = $_POST['email'];
+	 	$objet = $_POST['objet'];
+	 	$mesg = $_POST['msg'];
  			
  		$sendmail = $bdd->prepare("INSERT INTO `mailbox` (`id_mail`, `name_sender`, `email_sender`, `subject`, `message`, `time`) VALUES (NULL,?,?,?,?, current_timestamp());");
- 		$sendmail->execute(array($nom, $email, $objet, $msg));
+ 		$sendmail->execute(array($nom, $email, $objet, $mesg));
+ 		   $success = 1;
+       	   $msg = "";
 
- 			$msgmaintso = "<b>Votre message a été bien envoyer✔</b>";
+		}
 
-
- 			
-
-
-
- 		}
- }
- else {
- 	$msgmena = "<b> Tous les champs doivent être remplient❗</b>";
- }
-}
+    $res = ["success" => $success, "msg" => $msg];
+    echo json_encode($res);
