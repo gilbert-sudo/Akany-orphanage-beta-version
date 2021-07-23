181cfdf3fd5d7088019a620537c610e1bf412fb2
gilbert-sudo <mikamickswonder@gmail.com>
2021-07-23 13:52:44 +0300

    index

--- index.php
+++ index.php
@@ -42,7 +42,7 @@ session_start();
     
     <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light site-navbar-target" id="ftco-navbar">
       <div class="container">
-      <a class="navbar-brand" href="index.php"><img src="images/logo.png" alt="" style="width: 50px;">&nbsp;Akany Tafita/Sahasoa</a>
+      <a class="navbar-brand" href="index.php"><img src="images/logo.png" alt="" style="width: 50px;">&nbsp;Akany Tafita</a>
         <button class="navbar-toggler js-fh5co-nav-toggle fh5co-nav-toggle" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="oi oi-menu"></span> Menu
         </button>
